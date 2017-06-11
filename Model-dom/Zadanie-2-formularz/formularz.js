2a3c38c316ae1b1648dee012f694fb736371b750
Michał Jarząbek <nebraska994@gmail.com>
2017-06-11 16:43:21 +0200

    finish zad 2

--- Model-dom/Zadanie-2-formularz/formularz.js
+++ Model-dom/Zadanie-2-formularz/formularz.js
@@ -1,2 +1,20 @@
-'use strict'
+'use strict';
 
+
+
+function pobierzImie(event) {
+	event.preventDefault();
+	var fname = document.getElementsByName('fname')[0].value;
+	var lname = document.getElementsByName('lname')[0].value;
+	console.log('BLOCK');
+	console.log(fname);
+	console.log(lname);
+
+}
+
+
+function wyswietlWK() {
+	console.log(fname);
+	console.log(lname);
+}
+var form = document.getElementById('przycisk');
